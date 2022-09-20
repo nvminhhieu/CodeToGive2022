6f1bc6fd54f7f3f4a6d954ac57763ea47a1bcc26
Toby <nvminhhieu1999@gmail.com>
2022-09-20 14:19:56 +0200

    Add prettier

--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -3,29 +3,29 @@ import Image from 'next/image';
 import { ReactElement } from 'react';
 
 type Props = {
-	data: any;
+  data: any;
 };
 
 const Test = ({ data }: Props): ReactElement | null => {
-	console.log(data);
-	return (
-		<>
-			<div className=''>This is another test page test 2</div>
-			<h1>{data?.message}</h1>
-			<Image src='' alt='test' layout='fill' />
-		</>
-	);
+  console.log(data);
+  return (
+    <>
+      <div className="">This is another test page test 2</div>
+      <h1>{data?.message}</h1>
+      <Image src="" alt="test" layout="fill" />
+    </>
+  );
 };
 
 export default Test;
 
 export const getStaticProps: GetStaticProps = async () => {
-	const request = await fetch(`${process.env.HOST}/api/v1/hello`);
-	const response = await request.json();
+  const request = await fetch(`${process.env.HOST}/api/v1/hello`);
+  const response = await request.json();
 
-	return {
-		props: {
-			data: response,
-		},
-	};
+  return {
+    props: {
+      data: response,
+    },
+  };
 };
