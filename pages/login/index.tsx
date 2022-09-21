22dbaa04a897f15c89b5a3f469cc19b661eb8720
Toby <nvminhhieu1999@gmail.com>
2022-09-21 13:16:01 +0200

    Draft login form

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,5 +1,30 @@
 const Login = () => {
-  return <></>
+  const { control, handleSubmit } = useForm()
+  const onSubmit = (data: any) => console.log(data)
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <CustomTextField
+        control={control}
+        name="email"
+        label="E-mail address"
+        type="email"
+        variant="outlined"
+        sx={{ margin: '15px 0' }}
+      />
+      <CustomTextField
+        control={control}
+        name="phone"
+        label="Phone"
+        type="phone"
+        variant="outlined"
+      />
+
+      <CustomButton variant="outlined" type="submit" sx={{ marginTop: '15px' }}>
+        Submit
+      </CustomButton>
+    </form>
+  )
 }
 
 export default Login
