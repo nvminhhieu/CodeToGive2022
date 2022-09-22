c32594682da74f8fceaf701f3470a45ca7995359
Toby <nvminhhieu1999@gmail.com>
2022-09-22 18:41:06 +0200

    create component

---
+++ components/common/PageTitle/index.tsx
@@ -0,0 +1,35 @@
+import styled from "@emotion/styled"
+import { ReactElement } from "react"
+
+type Props = {
+  title: string
+  description?: string | ReactElement
+}
+
+const PageTitle = ({ title, description }: Props) => {
+  return (
+    <Container>
+      <Title>{title}</Title>
+      <Description>{description ? description : ""}</Description>
+    </Container>
+  )
+}
+
+export default PageTitle
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`
+
+const Title = styled.h2`
+  font-size: 68px;
+  font-weight: 600;
+  color: #191e28;
+`
+
+const Description = styled.p`
+  font-size: 24px;
+  color: #6a6e77;
+`
--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -1,6 +1,7 @@
 import { GetStaticProps } from "next"
 import Image from "next/image"
 import { ReactElement } from "react"
+import PageTitle from "../../components/common/PageTitle"
 
 type Props = {
   data: any
@@ -10,6 +11,16 @@ const Test = ({ data }: Props): ReactElement | null => {
   console.log(data)
   return (
     <>
+      <PageTitle
+        title="Assessment"
+        description={
+          <>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+            <br />
+            minima nihil unde nulla, modi corporis iure libero soluta saepe rem!
+          </>
+        }
+      />
       <div className="">This is another test page test 2</div>
       <h1>{data?.message}</h1>
       <Image src="" alt="test" layout="fill" />
