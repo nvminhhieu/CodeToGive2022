211f678baef082bbb2b86ad051ec6ce1634a9bbf
Toby <52330522+nvminhhieu@users.noreply.github.com>
2022-09-23 00:36:06 +0200

    1st api mock

---
+++ data/suggested_job.ts
@@ -0,0 +1,12 @@
+export const suggestedJobs = [
+  {
+    label: "shop assistance",
+    match_value: 69,
+    details: {
+      company_name: "Inc",
+      about: "this company is doing blah blah",
+      description: "to success you should have:",
+    },
+    image: "./../",
+  },
+]
