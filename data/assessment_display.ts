e80d9e5e476fe6fcc8be9f77b01c68822175861f
Gyimesi Regina <gyimesiregina@Gyimesi-MacBook-Air.local>
2022-09-23 23:24:50 +0100

    assessment mock

---
+++ data/assessment_display.ts
@@ -0,0 +1,44 @@
+export const assessments = [
+  {
+    title: "Work motivation test",
+    description:
+      "Tell more about your interests so you can find the most suitable job.",
+    completed: false,
+    icon: {
+      name: "suitcase",
+      color: "green",
+    },
+    progress: 0,
+  },
+  {
+    title: "English language test",
+    description:
+      "You will be asked some questions about English situations to measure your knowledge.",
+    completed: false,
+    icon: {
+      name: "language",
+      color: "pink",
+    },
+    progress: 0,
+  },
+  {
+    title: "Visio-perceptual skills",
+    description: "We will measure your sight and hearing.",
+    completed: false,
+    icon: {
+      name: "eye",
+      color: "blue",
+    },
+    progress: 0,
+  },
+  {
+    title: "Some other test",
+    description: "This is some other random test to observe your capabilities.",
+    completed: false,
+    icon: {
+      name: "questionmark",
+      color: "brown",
+    },
+    progress: 0,
+  },
+]
---
+++ pages/assessments/index.tsx
@@ -0,0 +1,15 @@
+import PageTitle from "../../components/Common/PageTitle"
+import Layout from "../../components/Layout"
+
+const AssessmentPage = () => {
+  return (
+    <Layout>
+      <PageTitle
+        title="Assessments"
+        description="Here you can find the assessments required to get a job. Please select the test you would like to do."
+      />
+    </Layout>
+  )
+}
+
+export default AssessmentPage
