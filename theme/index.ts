fcd60cb4301ff841decdbaadbc672f43f6bae15d
Toby <52330522+nvminhhieu@users.noreply.github.com>
2022-09-24 01:25:26 +0200

    Add Theme MUI

---
+++ theme/index.ts
@@ -0,0 +1,21 @@
+import { createTheme } from "@mui/material/styles"
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#0097F2",
+      dark: "#1689d2",
+    },
+    secondary: {
+      main: "#FF9700",
+      contrastText: "#ffffff",
+    },
+    success: {
+      main: "#4DC56E",
+      contrastText: "#ffffff",
+    },
+    error: {
+      main: "#f54d43",
+    },
+  },
+})
