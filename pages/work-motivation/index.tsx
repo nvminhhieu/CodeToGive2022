11c0c2475e06d9c2bca5cd36b5d1b194baa0e71b
Toby <52330522+nvminhhieu@users.noreply.github.com>
2022-09-23 20:34:19 +0200

    add work motivation page

---
+++ pages/work-motivation/index.tsx
@@ -0,0 +1,20 @@
+import PageTitle from "../../components/common/PageTitle"
+import Layout from "../../components/Layout"
+
+const WorkMotivation = () => {
+  return (
+    <Layout>
+      <PageTitle
+        title="Work motivation test"
+        description={
+          <>
+            Tell more about your interests so you can find the most suitable
+            job.
+          </>
+        }
+      />
+    </Layout>
+  )
+}
+
+export default WorkMotivation
