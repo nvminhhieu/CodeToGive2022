4ebe39b1660ad612123f31708c4080059c5c03d9
Toby <nvminhhieu1999@gmail.com>
2022-09-29 13:00:03 +0200

    Add custom Slider component

--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -1,19 +1,44 @@
-import { Alert } from "@mui/material"
+import { Alert, Slider } from "@mui/material"
 import { GetStaticProps } from "next"
 import { ReactElement, useState } from "react"
 import PageTitle from "../../components/common/PageTitle"
 import Layout from "../../components/Layout"
 import { useSession } from "next-auth/react"
+import { useForm } from "react-hook-form"
 
 type Props = {
   data: any
 }
+
+const marks = [
+  {
+    value: 1,
+    label: "1",
+  },
+  {
+    value: 2,
+    label: "2",
+  },
+  {
+    value: 3,
+    label: "3",
+  },
+  {
+    value: 4,
+    label: "4",
+  },
+  {
+    value: 5,
+    label: "5",
+  },
+]
 
 const Test = ({ data }: Props): ReactElement | null => {
   const [alertIsOpen, setAlertIsOpen] = useState(true)
   console.log(data)
   const session = useSession()
   console.log("session", session)
+
   return (
     <Layout>
       <PageTitle
