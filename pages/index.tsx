a2b78681be59656cdec0ecf7d6c1892b1f40e93c
Toby <nvminhhieu1999@gmail.com>
2022-10-04 19:18:29 +0200

    Link home assessment to generated uuid

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,16 @@
 import type { NextPage } from "next"
 import { useRouter } from "next/router"
+import { useUUIDContext } from "../context/UUIDContext"
 import styles from "../styles/Home.module.css"
 
 const Home: NextPage = () => {
   const router = useRouter()
+  const { uuid } = useUUIDContext()
   return (
     <div
       className={styles.container}
       onClick={() => {
-        router.push("/assessments")
+        router.push(`${uuid}`)
       }}
     >
       To assessments page
