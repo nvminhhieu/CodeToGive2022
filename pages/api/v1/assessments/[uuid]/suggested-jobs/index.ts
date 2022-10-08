89df7fb768071003d729c313e3eb825fe51086f3
Toby <52330522+nvminhhieu@users.noreply.github.com>
2022-10-08 21:06:42 +0200

    suggested job for report

--- pages/api/v1/assessments/[uuid]/suggested-jobs/index.ts
+++ pages/api/v1/assessments/[uuid]/suggested-jobs/index.ts
@@ -1,11 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next"
 import IJob from "../../../../../../types/job"
-import { assessments as mock_display_tests } from "../../../../../data/assessment_display"
-import { Assessment, ITestDisplay } from "../../../../../types/assessment"
-
-interface DetailedAssessment extends Assessment {
-  tests: ITestDisplay[]
-}
 
 // Use to display the assessment, at /[uuid] page
 
@@ -14,6 +8,7 @@ const listMockSuggestedJobs: IJob[] = [
     job_id: 2,
     title: "Recptionist",
     labels: null,
+    match_score: 7,
     image:
       "https://images.unsplash.com/photo-1556741533-6e6a62bd8b49?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmVjZXB0aW9uaXN0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
   },
@@ -21,6 +16,7 @@ const listMockSuggestedJobs: IJob[] = [
     job_id: 3,
     title: "Bartender",
     labels: null,
+    match_score: 8,
     image:
       "https://images.unsplash.com/photo-1595751866979-de6e9d606220?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YmFydGVuZGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
   },
@@ -28,6 +24,7 @@ const listMockSuggestedJobs: IJob[] = [
     job_id: 1,
     title: "Librarian",
     labels: null,
+    match_score: 12,
     image:
       "https://images.unsplash.com/photo-1507842217343-583bb7270b66?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bGlicmFyeXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
   },
@@ -35,6 +32,7 @@ const listMockSuggestedJobs: IJob[] = [
     job_id: 4,
     title: "Software Engineer",
     labels: null,
+    match_score: 13,
     image:
       "https://images.unsplash.com/photo-1534665482403-a909d0d97c67?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
   },
@@ -42,6 +40,7 @@ const listMockSuggestedJobs: IJob[] = [
     job_id: 5,
     title: "Barista",
     labels: null,
+    match_score: 8,
     image:
       "https://images.unsplash.com/photo-1507914372368-b2b085b925a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
   },
